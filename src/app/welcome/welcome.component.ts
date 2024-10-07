b302b78d8f12926a154c058d813e1d99275f78f4
alfred91 <alfred.91.mc@gmail.com>
2024-10-07 18:34:46 +0200

    .

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -9,10 +9,12 @@ import { TmdbService } from '../tmdb.service';
 export class WelcomeComponent {
   constructor(private tmdbService: TmdbService) {}
 
-  // UTILIZA TMDBSERVICE PARA AUTENTICAR AL USUARIO
+  // Utiliza TMDBService para autenticar al usuario
   login(): void {
     this.tmdbService.getRequestToken().subscribe((response) => {
-      window.location.href = `https://www.themoviedb.org/authenticate/${response.request_token}?redirect_to=http://localhost:4200/auth-callback`;
+      // Cambia la URL de redirección a tu aplicación en GitHub Pages
+      const redirectUrl = 'https://alfred91.github.io/pelis-app/auth-callback';
+      window.location.href = `https://www.themoviedb.org/authenticate/${response.request_token}?redirect_to=${redirectUrl}`;
     });
   }
 }
